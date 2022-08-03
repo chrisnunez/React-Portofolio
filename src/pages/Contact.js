0697d13b7178f7ff1a57c591186c3219d707d39f
Chris <nunezchristian92@gmail.com>
2022-08-02 19:18:36 -0700

    modified contact css

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -8,9 +8,9 @@ class Contact extends Component {
             <div>
                 <div className="text-center bg-white pt-5 contact">
                     <Container className="">
-                        <Row className="justify-content-md-center">
-                            <Col xs={2}><a href="https://github.com/chrisnunez" className="text-decoration-none githubIcon"><i class= "fab fa-github"></i></a></Col>
-                            <Col xs={2}><a href="https://www.linkedin.com/in/christian-nunez-04491818b/" className="linkedinIcon"><i class="fab fa-linkedin-in"></i></a></Col>
+                        <Row className="justify-content-md-center parent">
+                            <Col className="child" xs={2}><a href="https://github.com/chrisnunez" className="text-decoration-none githubIcon child"><i class= "fab fa-github"></i></a></Col>
+                            <Col className="child" xs={2}><a href="https://www.linkedin.com/in/christian-nunez-04491818b/" className="linkedinIcon child"><i class="fab fa-linkedin-in"></i></a></Col>
                         </Row>
                     </Container>
                     <h3>Have a question or want to work together?</h3>
